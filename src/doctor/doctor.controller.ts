64d52447fc6c05138e154d6674d1fe796ff71663
Piyush Prajapati <piyushprajapati7120@gmail.com>
2026-06-10 18:05:27 +0530

    fix: restore missing doctor module from day 4

---
+++ src/doctor/doctor.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { DoctorService } from './doctor.service';
+import { GetDoctorsFilterDto } from './dto/get-doctor-filter.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { ParseUUIDPipe } from '@nestjs/common';
+@Controller('doctor')
+@UseGuards(AuthGuard('jwt')) 
+export class DoctorController {
+  constructor(private readonly doctorService: DoctorService) {}
+
+  @Get()
+  getDoctors(@Query() filterDto: GetDoctorsFilterDto) {
+    return this.doctorService.getDoctors(filterDto);
+  }
+
+  @Get(':id')
+  getDoctorById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.doctorService.getDoctorById(id);
+  }
+}
